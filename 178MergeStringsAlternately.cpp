4c70a8557af7be0774ad10ee99f33c2480dc458f
saqttra <113034629+saqttra@users.noreply.github.com>
2025-03-19 19:23:06 -0600

    opt [T1] main: optimize problem 178 solution

--- 178MergeStringsAlternately.cpp
+++ 178MergeStringsAlternately.cpp
@@ -1,10 +1,19 @@
 // Problem 178
-// Solved on Wed 19 Mar, 2025 by Saqttra
+// Solved on Wed 19 Mar, 2025 by Saqttra (First Attempt)
+// Last attempt on Wed 19 Mar, 2025 by Saqttra
 
 #include <iostream>
 
 class Solution{
   public:
+/*  
+    My first attempt, based on:
+
+      If a, b ∈ R => when comparing them, the following may happen:
+        a > b
+        a < b
+        a = b
+
     std::string mergeAlternately(std::string word1, std::string word2){
       std::string newStr;
       newStr.reserve(word1.size() + word2.size());
@@ -33,6 +42,24 @@ class Solution{
         newStr.push_back(word1[i]);
         newStr.push_back(word2[i]);
       }
+      return newStr;
+    }
+*/  
+    // Optimized attempt.
+    // Hint: Keep pushing chars until it reaches the limit on each.
+    // However, no matter the size of each str, the str1 always comes first.
+    std::string mergeAlternately(std::string word1, std::string word2){
+      size_t m = word1.size(), n = word2.size();
+      std::string newStr;
+      newStr.reserve(m + n);
+
+      size_t i = 0;
+      while( i < (m + n)){
+        if(i < m) newStr.push_back(word1[i]);
+        if(i < n) newStr.push_back(word2[i]);
+        i++;
+      }
+
       return newStr;
     }
 };
